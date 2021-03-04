b0a69da5923c246127f7ef42a2b9574d47ae42fc
Jonáš Herzig <herzigj.03@in.spst.cz>
2021-03-04 12:01:55 +0100

    Přidejte soubory projektu.

---
+++ metody/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace metody
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double vzdalenost(int a1, int b1, int a2, int b2)
+        {
+            return Math.Sqrt(Math.Pow(Math.Abs(a1 - a2), 2) + Math.Pow(Math.Abs(b1 - b2), 2));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a1 = Convert.ToInt32(textBox1.Text);
+            int b1 = Convert.ToInt32(textBox2.Text);
+            int a2 = Convert.ToInt32(textBox3.Text);
+            int b2 = Convert.ToInt32(textBox4.Text);
+            label1.Text = vzdalenost(a1, b1, a2, b2).ToString();
+        }
+    }
+}
